744faa844d247e85901d4912baeec1af6ee9646e
Nobound <jean.ungerer.nobound@gmail.com>
2023-09-06 16:45:42 +0200

    added dockerfiles

--- client-ui/src/main/java/com/clientui/clientui/ClientUiApplication.java
+++ client-ui/src/main/java/com/clientui/clientui/ClientUiApplication.java
@@ -16,3 +16,6 @@ public class ClientUiApplication {
 
 
 //  https://www.youtube.com/watch?v=lh1oQHXVSc0&list=PLSVW22jAG8pBnhAdq9S8BpLnZ0_jVBj0c&index=2
+//  https://medium.com/geekculture/how-jwt-is-implemented-in-api-gateway-on-microservice-architecture-5dce8f5b89aa
+//  https://geekflare.com/fr/securing-microservices/
+//  https://springframework.guru/jwt-authentication-in-spring-microservices-jwt-token/
